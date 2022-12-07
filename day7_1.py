31f7f51b81576ccdd016411e1f5146e1ba575b63
David Eynon <davideynon@davids-macbook-pro.home>
2022-12-07 18:42:13 +0100

    day 7 part 1 - tricky. Saving progress of hierarchy trie tree navigation

---
+++ day7_1.py
@@ -0,0 +1,72 @@
+"""
+NOTES:
+    -trie of dictionaries?
+        -how to find parent when ../
+
+
+"""
+import json
+from baseclass import BaseClass
+
+class Day7Part1(BaseClass):
+
+    def __init__(self):
+        self.dt = self.get_data('day7_test')
+        self.root = {}
+
+    def find_node(self, node):
+        if node is None:
+            return node
+        stack = [self.root]
+        while stack:
+            nd = stack.pop()
+            if node in nd:
+                return nd[node]
+            else:
+                for edge in nd:
+                    if edge != "size":
+                        stack.append(nd[edge])
+
+
+    def main(self):
+
+        """ create directory structure (trie) """
+        curr = self.root
+        curr_label = None
+
+        for line in self.dt:
+            cmd = line.split()
+
+            if cmd[0] == "$":
+                
+                if cmd[1] == "cd":
+
+                    to_dir = cmd[2]
+                    
+                    if to_dir != "..":
+                        
+                        if to_dir not in curr:
+                            # set
+                            curr[to_dir] = {}
+                        curr = curr[to_dir]
+                        curr['parent'] = curr_label
+                        if 'label' not in curr:
+                            curr['label'] = to_dir
+                            curr_label = curr['label']
+
+                    else: # cd ..
+                        if curr['parent'] is not None:
+                            curr = self.find_node(curr['parent'])
+                            curr_label = curr['label']
+                            if curr:
+                                prev_label = curr['parent']
+                            else:
+                                curr = self.root
+
+        print(json.dumps(self.root,indent=2))
+
+
+
+
+if __name__=="__main__":
+    Day7Part1().main()
---
+++ day7_2.py
@@ -0,0 +1,6 @@
+from baseclass import BaseClass
+
+class Day7Part2(BaseClass):
+
+    def __init__(self):
+        self.dt = self.get_data('day7')
