f33cf54af72d403447c2311b70d35a4b1f26655e
David Eynon <davideynon@davids-macbook-pro.home>
2022-12-07 21:48:50 +0100

    redo, data navigation squared down

--- day7_1.py
+++ day7_1.py
@@ -1,81 +1,71 @@
 """
 NOTES:
-    -trie of dictionaries?
-        -how to find parent when ../
-
+    -trie of dictionaries
+    -each trie has
+        -ds (directory structure) which is a stack
+            -popped to move up directory
+            -when created, the dir pushed to ds
+        -size (file size)
+        -files (list) to ensure uniqueness
+            -edge case: ls directory again
+                -ls is where i compile the files and sizes for that directory
 
 """
+
+from collections import deque
+import copy
 import json
 from baseclass import BaseClass
 
 class Day7Part1(BaseClass):
 
     def __init__(self):
-        self.dt = self.get_data('day7_test')
-        self.root = {}
-
-    def find_node(self, node):
-        if node is None:
-            return node
-        stack = [self.root]
-        while stack:
-            nd = stack.pop()
-            if node in nd:
-                return nd[node]
-            else:
-                for edge in nd:
-                    if edge != "size":
-                        stack.append(nd[edge])
+        self.dt = self.get_data('day7')
+        self.trie = {}
+        self.levels = 0
 
 
-    def main(self):
+    def find_node(self, file_struct):
+        head = self.trie
+        for f in file_struct:
+            head = head[f]
+        return head
 
+    def form_trie(self):
         """ create directory structure (trie) """
-        curr = self.root
-        curr_label = None
+        node = self.trie
 
         for line in self.dt:
             cmd = line.split()
 
-            if cmd[0] == "$":
+            # cd forward
+            if cmd[0] == "$" and cmd[1] == "cd" and cmd[2] != "..":
+                new_dir = cmd[2]
                 
-                if cmd[1] == "cd":
+                if 'full_dir' not in node:
+                    node['full_dir'] = []
 
-                    to_dir = cmd[2]
-                    
-                    if to_dir != "..":
-                        
-                        if to_dir not in curr:
-                            # set
-                            curr[to_dir] = {}
-                        curr = curr[to_dir]
-                        curr['parent'] = curr_label
-                        if 'label' not in curr:
-                            curr['label'] = to_dir
-                            curr_label = curr['label']
+                if new_dir not in node:
+                    old_full_dir = node['full_dir']
+                    node[new_dir] = {}
+                node = node[new_dir]
+                
+                if 'full_dir' not in node:
+                    node['full_dir'] = old_full_dir + [new_dir]
 
-                    else: # cd ..
-                        if curr['parent'] is not None:
-                            curr = self.find_node(curr['parent'])
-                            curr_label = curr['label']
-                            if curr:
-                                prev_label = curr['parent']
-                            else:
-                                curr = self.root
-            else: # implied as ls
-                if cmd[0] != 'dir':
-                    if 'total_size' not in curr:
-                        curr['total_size'] = 0
-                    if 'files' not in curr: 
-                        curr['files'] = []
-                    if cmd[1] not in curr['files']:
-                        # bet you there are multiple ls's 
-                        curr['total_size'] += int(cmd[0])
-                        curr['files'].append(cmd[1])
+                self.levels += 1
+
+            # cd back
+            if cmd[0] == "$" and cmd[1] == "cd" and cmd[2] == ".." and self.levels > 1:
+                old_dir = node['full_dir'][:]
+                old_dir.pop()
+                node = self.find_node(old_dir)
+                self.levels -= 1
 
 
-        print(json.dumps(self.root,indent=2))
-
+    def main(self):
+        self.form_trie()
+        print(json.dumps(self.trie, indent=2))
 
 
 
