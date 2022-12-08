7895e9becd99b074a099d78bfeb6497ba6a3c456
David Eynon <davideynon@davids-macbook-pro.home>
2022-12-08 23:03:27 +0100

    day 7 revisit in progress

--- day7_1.py
+++ day7_1.py
@@ -17,72 +17,86 @@ import copy
 import json
 from baseclass import BaseClass
 
-class Node:
-    def __init__(self, val=None):
-        self.val = val
-        self.parent = None
-        self.children = {} #directory name: object
-        self.size = 0
-        self.files = []
-
-
 class Day7Part1(BaseClass):
 
     def __init__(self):
-        self.dt = self.get_data('day7_test')
-        self.head = Node()
-        self.values = {}
+        self.dt = self.get_data('day7')
+        self.tree = {}
+        """ 
+            {
+                '[name]':{
+                            'size': int,
+                            'parent': dir ,
+                            'filenames':[]
 
+                            },
+                ...
+            }
+        """
+        self.mapping = {}
 
     def main(self):
-        curr = self.head
+        curr = self.tree
+        curr_listing = None
 
         for line in self.dt:
             cmd = line.split()
 
             # cd forward
+
             if cmd[0] == "$" and cmd[1] == "cd" and cmd[2] != "..":
                 new_dir = cmd[2]
-                if new_dir not in curr.children:
-                    new_node = Node(new_dir)
-                    new_node.parent = curr
-                    curr.children[new_dir] = new_node
-                    curr = new_node
-                else:
-                    curr = curr.children[new_dir]
+
+                if new_dir not in curr:
+                    curr[new_dir] = {}
+                    curr = curr[new_dir]
+                    self.mapping[new_dir] = curr
+                    curr['size'] = 0
+                    curr['parent'] = curr_listing
+                    curr_listing = new_dir
+                    curr['filenames'] = []
 
             # cd back
+
             elif cmd[0] == "$" and cmd[1] == "cd" and cmd[2] == "..":
-                if curr.parent is not None:
-                    curr = curr.parent
+                curr = self.mapping[curr['parent']]
 
-            # listing
-            elif cmd[0] != "$" and cmd[0] != "dir":
-                size, filename = cmd[0], cmd[1]
-                if filename not in curr.files:
-                    curr.size += int(size)
-                    curr.files.append(filename)
-                    self.values[curr.val] = curr.size
+           # listing
 
-        print(self.recurse(self.head))
-        print(self.values)
-        total = 0
-        for key in self.values:
-            if self.values[key] <= 100000:
-                total += self.values[key]
-        print(total)
+            elif cmd[0] != "$" and cmd[0] != 'dir':
+                size, filename = int(cmd[0]), cmd[1]
+                if filename not in curr['filenames']:
+                    curr['size'] += size
+                    curr['filenames'].append(filename)
 
-    def recurse(self, node): 
-        curr_val = node.size
-        if not node.children:
-            return int(curr_val)
-        total = curr_val
-        for key in node.children:
-            total += self.recurse(node.children[key])
-        return total
+
+
+
+
+        #print(json.dumps(self.tree, indent=2))
+
+        answer = 0
+        for dirs in self.mapping:
+            total = 0
+            stack = [dirs]
+            while stack:
+                dir_name = stack.pop()
+                directory = self.mapping[dir_name]
+                total += directory['size']
+                for edge in directory:
+                    if edge not in ('filenames','size','parent'):
+                        stack.append(edge)
+            print(dirs, total)
+            if total <= 100000:
+                answer += total
+        print(answer)
+
+
+
 
 
 
 if __name__=="__main__":
     day7 = Day7Part1()
     day7.main()
+    # 1137314 - too low
